7ad112f3305575fe806855ff37360b6f8322f1cc
Christian daheim <auriocus@gmx.de>
2016-05-04 16:31:59 +0200

    Add Tcl library for synchronous settig of multiple PVs

---
+++ asynca.tcl
@@ -0,0 +1,61 @@
+namespace eval ::AsynCA {
+	
+	variable ns [namespace current]
+	variable managed_PVs {}
+
+	proc connectwait {args} {
+		variable ns
+		variable managed_PVs
+
+		set result {}
+		foreach p $args {
+			set PV [connect $p -command ${ns}::connectcall]
+			dict set managed_PVs $p {}
+			dict set result $p $PV
+		}
+
+		while {![allconnected]} {
+			vwait ${ns}::managed_PVs
+		}
+
+		return $result 
+	}
+
+	proc connectcall {PV status} {
+		variable managed_PVs
+		dict set managed_PVs [$PV name] $PV
+	}
+
+
+	proc allconnected {} {
+		variable managed_PVs
+		expr {[dict size [dict filter $managed_PVs value {}]] == 0}
+	}
+
+	
+	variable pendingwrites {}
+	proc putwait {PVs values} {
+		variable ns
+		variable pendingwrites
+
+		foreach p $PVs v $values {
+			$p put $v -command [list ${ns}::writefinish $p]
+			dict set pendingwrites $p 1
+		}
+
+		while {![allwritesfinished]} {
+			vwait ${ns}::pendingwrites
+		}
+	
+	}
+
+	proc allwritesfinished {} {
+		variable pendingwrites
+		expr {[dict size [dict filter $pendingwrites value 1]] == 0}
+	}
+
+	proc writefinish {pv args} {
+		variable pendingwrites
+		dict set pendingwrites $pv 0
+	}
+}
